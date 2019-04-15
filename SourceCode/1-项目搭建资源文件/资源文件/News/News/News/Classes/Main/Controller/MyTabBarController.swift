5c24b96ebb1fa5b30129c85b72793343f090d748
CoderDream <coderdream@gmail.com>
2019-04-15 10:07:33 +0800

    1-项目搭建资源文件

---
+++ SourceCode/1-项目搭建资源文件/资源文件/News/News/News/Classes/Main/Controller/MyTabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  MyTabBarController.swift
+//  News
+//
+//  Created by 杨蒙 on 2017/9/6.
+//  Copyright © 2017年 hrscy. All rights reserved.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(tabBar.subviews)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tabbar = UITabBar.appearance()
+        tabbar.tintColor = UIColor(red: 245 / 255.0, green: 90 / 255.0, blue: 93 / 255.0, alpha: 1.0)
+        
+        // 添加子控制器
+        addChildViewControllers()
+    }
+    
+    /// 添加子控制器
+    func addChildViewControllers() {
+        setChildViewController(HomeViewController(), title: "首页", imageName: "home_tabbar_32x32_", selectedImageName: "home_tabbar_press_32x32_")
+        setChildViewController(VideoViewController(), title: "视频", imageName: "video_tabbar_32x32_", selectedImageName: "video_tabbar_press_32x32_")
+        setChildViewController(HuoshanViewController(), title: "小视频", imageName: "huoshan_tabbar_32x32_", selectedImageName: "huoshan_tabbar_press_32x32_")
+        setChildViewController(MineViewController(), title: "我的", imageName: "mine_tabbar_32x32_", selectedImageName: "mine_tabbar_press_32x32_")
+        // tabBar 是 readonly 属性，不能直接修改，利用 KVC 把 readonly 属性的权限改过来
+        setValue(MyTabBar(), forKey: "tabBar")
+    }
+    
+    /// 初始化子控制器
+    func setChildViewController(_ childController: UIViewController, title: String, imageName: String, selectedImageName: String) {
+        // 设置 tabbar 文字和图片
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: selectedImageName)
+        childController.title = title
+        // 添加导航控制器为 TabBarController 的子控制器
+        let navVc = MyNavigationController(rootViewController: childController)
+        addChildViewController(navVc)
+    }
+    
+}

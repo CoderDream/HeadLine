a1456bd4279e035b0457ee7ea729d773b29b9f57
CoderDream <coderdream@gmail.com>
2019-04-16 21:01:41 +0800

    创建简单的TableView 20190416

--- 02/0202/News/News/Classes/Mine/Controller/MineViewController.swift
+++ 02/0202/News/News/Classes/Mine/Controller/MineViewController.swift
@@ -14,13 +14,28 @@ class MineViewController: UITableViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.red
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = UIColor.globalBackgroundColor()
     }
 }
 
 extension MineViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: 10))
+        view.backgroundColor = UIColor.globalBackgroundColor()
+        return view
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
---
+++ 02/0202/News/News/Classes/Others/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  News
+//
+//  Created by CoderDream on 2019/4/16.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+/// 屏幕的宽度
+let SCREEN_WIDTH = UIScreen.main.bounds.width
+/// 屏幕的宽度
+let SCREEN_HEIGHT = UIScreen.main.bounds.height

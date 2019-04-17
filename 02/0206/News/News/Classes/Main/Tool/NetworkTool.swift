46a96a8f036c3092e1d930d8db10499fae502f36
CoderDream <coderdream@gmail.com>
2019-04-17 14:04:35 +0800

    206-自定义我的界面cell 20190417

---
+++ 02/0206/News/News/Classes/Main/Tool/NetworkTool.swift
@@ -0,0 +1,64 @@
+//
+//  NetworkTool.swift
+//  News
+//
+//  Created by CoderDream on 2019/4/16.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+protocol NetworkToolProtocol {
+    // ---------------------------- 我的 mine ----------------------------------
+    // 我的界面 cell 的数据
+    static func loadMyCellData(completionHandler: @escaping (_ sections: [[MyCellModel]]) -> ())
+    // 我的关注数据
+    static func loadMyConcern()
+}
+
+extension NetworkToolProtocol {
+    // ---------------------------- 我的 mine ----------------------------------
+    // 我的界面 cell 的数据
+    static func loadMyCellData(completionHandler: @escaping (_ sections: [[MyCellModel]]) -> ()) {
+        let url = BASE_URL + "/user/tab/tabs/?"
+        let params = ["device_id": device_id]
+        Alamofire.request(url, method: .get, parameters: params).responseJSON{ (response) in
+            guard response.result.isSuccess else {
+                // 网络错误的提示信息
+                return
+            }
+            if let value = response.result.value {
+                let json = JSON(value)
+                guard json["message"] == "success" else {
+                    return
+                }
+                if let data = json["data"].dictionary {
+                    print(data)
+                    if let sections = data["sections"]?.array {
+                        var sectionArray = [[MyCellModel]]()
+                        for item in sections {
+                            var rows = [MyCellModel]()
+                            for row in item.arrayObject! {
+                                let myCellModel = MyCellModel.deserialize(from: row as? NSDictionary)
+                                rows.append(myCellModel!)
+                            }
+                            sectionArray.append(rows)
+                        }
+                        completionHandler(sectionArray)
+                    }
+                }
+            }
+        }
+    }
+    // 我的关注数据
+    static func loadMyConcern() {
+        
+    }
+}
+
+
+struct NetworkTool: NetworkToolProtocol {
+    
+}
---
+++ 02/0206/News/News/Classes/Mine/Controller/MineViewController.swift
@@ -0,0 +1,76 @@
+//
+//  MineViewController.swift
+//  News
+//
+//  Created by CoderDream on 2019/4/15.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+class MineViewController: UITableViewController {
+    // 这里不能声明为可选类型，否则程序会崩溃
+    var sections = [[MyCellModel]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = UIColor.globalBackgroundColor()
+        // 使用注册方式的 TableCell
+        tableView.register(UINib(nibName: String(describing: MyOtherCell.self), bundle: nil), forCellReuseIdentifier: String(describing: MyOtherCell.self))
+        // 
+        NetworkTool.loadMyCellData { (sections) in
+            let string = "{\"text\":\"我的关注\",\"grey_text\":\"\"}"
+            let myConcern = MyCellModel.deserialize(from: string)
+            var myConcerns = [MyCellModel]()
+            myConcerns.append(myConcern!)
+            self.sections.append(myConcerns)
+            self.sections += sections
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension MineViewController {
+    // 每组头部的高度
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    // 每组头部视图
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: 10))
+        view.backgroundColor = UIColor.globalBackgroundColor()
+        return view
+    }
+    
+    // 组数
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    // 每组的行数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].count
+    }
+    
+    // cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: MyOtherCell.self)) as! MyOtherCell
+        let section = sections[indexPath.section]
+        let myCellModel = section[indexPath.row]
+        //cell.textLabel?.text = myCellModel.text
+        cell.leftLabel.text = myCellModel.text
+        cell.rightLabel.text = myCellModel.grey_text
+        return cell
+    }
+    
+    // 去掉点击 Cell 产生的阴影
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}

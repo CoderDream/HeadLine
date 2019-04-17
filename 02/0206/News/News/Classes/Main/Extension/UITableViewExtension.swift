c3c58e86d9dfbdee8811eb87b7543e429e17f973
CoderDream <coderdream@gmail.com>
2019-04-17 14:47:24 +0800

    0306 init

---
+++ 02/0206/News/News/Classes/Main/Extension/UITableViewExtension.swift
@@ -0,0 +1,27 @@
+//
+//  UITableViewExtension.swift
+//  News
+//
+//  Created by CoderDream on 2019/4/17.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    /// 注册 cell 的方法
+    func ymRegisterCell<T: UITableViewCell>(cell: T.Type) where T: RigisterCellOrNib {
+        if let nib = T.nib {
+            register(nib, forCellReuseIdentifier: T.identifier)
+        } else {
+            register(cell, forCellReuseIdentifier: T.identifier)
+        }
+    }
+    
+    /// 从缓存池出队已经存在的 cell
+    func ymDequeueReusableCell<T: UITableViewCell>(indexPath: IndexPath) -> T where T: RigisterCellOrNib {
+        return dequeueReusableCell(withIdentifier: T.identifier, for: indexPath) as! T
+    }
+    
+}

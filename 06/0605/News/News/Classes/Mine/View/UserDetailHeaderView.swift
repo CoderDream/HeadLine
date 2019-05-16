24cd374b7591d27d92d683834ab5ec3ad3a736fe
CoderDream <coderdream@gmail.com>
2019-05-16 21:31:36 +0800

    605-headerView 的点击事件(22分钟) init 20190516

---
+++ 06/0605/News/News/Classes/Mine/View/UserDetailHeaderView.swift
@@ -0,0 +1,129 @@
+//
+//  UserDetailHeaderView.swift
+//  News
+//
+//  Created by CoderDream on 2019/5/10.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+import IBAnimatable
+import Kingfisher
+
+class UserDetailHeaderView: UIView {
+    /// 背景图片
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    /// 背景图片顶部约束
+    @IBOutlet weak var bgImageViewTop: NSLayoutConstraint!
+    /// 用户头像
+    @IBOutlet weak var avatarImageView: UIImageView!
+    /// V 图标
+    @IBOutlet weak var vImageView: UIImageView!
+    /// 用户名
+    @IBOutlet weak var nameLabel: UILabel!
+    /// 头条号图标
+    @IBOutlet weak var toutiaohaoImageView: UIImageView!
+    /// 发私信按钮
+    @IBOutlet weak var sendMailButton: UIButton!
+    /// 关注按钮
+    @IBOutlet weak var concernButton: AnimatableButton!
+    /// 推荐按钮
+    @IBOutlet weak var recommendButton: AnimatableButton!
+    @IBOutlet weak var recommendButtonWidth: NSLayoutConstraint!
+    @IBOutlet weak var recommendButtonTrailing: NSLayoutConstraint!
+    /// 推荐 view
+    @IBOutlet weak var recommendView: UIView!
+    @IBOutlet weak var recommendViewHeight: NSLayoutConstraint!
+    /// 头条认证
+    @IBOutlet weak var verifiedAgencyLabel: UILabel!
+    @IBOutlet weak var verifiedAgencyLabelHeight: NSLayoutConstraint!
+    @IBOutlet weak var verifiedAgencyLabelTop: NSLayoutConstraint!
+    /// 认证内容
+    @IBOutlet weak var verifiedContentLabel: UILabel!
+    /// 地区
+    @IBOutlet weak var areaButton: UIButton!
+    @IBOutlet weak var areaButtonHeight: NSLayoutConstraint!
+    @IBOutlet weak var areaButtonTop: NSLayoutConstraint!
+    /// 描述内容
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionLabelHeight: NSLayoutConstraint!
+    /// 展开按钮
+    @IBOutlet weak var unfoldButton: UIButton!
+    @IBOutlet weak var unfoldButtonWidth: NSLayoutConstraint!
+    @IBOutlet weak var unfoldButtonTrailing: NSLayoutConstraint!
+    /// 关注数量
+    @IBOutlet weak var followingsCountLabel: UILabel!
+    /// 粉丝数量
+    @IBOutlet weak var followersCountLabel: UILabel!
+    
+    // 文章 视频 问答
+    @IBOutlet weak var topTabView: UIView!
+    @IBOutlet weak var topTabHeight: NSLayoutConstraint!
+    @IBOutlet weak var separatorView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    /// 底层 view
+    @IBOutlet weak var baseView: UIView!
+    /// 底部的 ScrollView
+    //@IBOutlet weak var bottomScrollView: UIScrollView!
+    
+    
+    var userDetail: UserDetail? {
+        didSet {
+            // 背景图片
+            backgroundImageView.kf.setImage(with: URL(string: userDetail!.bg_img_url)!)
+            // 头像
+            avatarImageView.kf.setImage(with: URL(string: userDetail!.avatar_url)!)
+            // VIP 图片
+            vImageView.isHidden = userDetail!.user_verified
+            nameLabel.text = userDetail!.screen_name
+            // 头条认证（如果为空，这约束（高度和top）设置为0，相当于隐藏）
+            if userDetail!.verified_agency == "" {
+                verifiedAgencyLabelHeight.constant = 0
+                verifiedAgencyLabelTop.constant = 0
+            } else {
+                verifiedAgencyLabel.text = userDetail!.verified_agency + "："
+                verifiedContentLabel.text = userDetail!.verified_content
+            }
+            // 关注
+            concernButton.isSelected = userDetail!.is_following
+            //
+            if userDetail!.area == "" {
+                areaButton.isHidden = true
+                areaButtonHeight.constant = 0
+                areaButtonTop.constant = 0
+            } else {
+                areaButton.setTitle(userDetail!.area, for: .normal)
+            }
+            // 描述
+            descriptionLabel.text = userDetail!.description
+            if userDetail!.descriptionHeight! > 21 {
+                unfoldButton.isHidden = false
+                unfoldButtonWidth.constant = 40
+            }
+            
+            //
+            followersCountLabel.text = userDetail!.followersCount
+            followingsCountLabel.text = userDetail!.followingsCount
+            layoutIfNeeded()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    class func headerView() -> UserDetailHeaderView {
+        return Bundle.main.loadNibNamed("\(self)", owner: nil, options: nil)?.last as! UserDetailHeaderView
+    }
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

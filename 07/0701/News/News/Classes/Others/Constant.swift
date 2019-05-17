a7a6894f070d3582236f466067829755345c92fe
CoderDream <coderdream@gmail.com>
2019-05-17 16:02:49 +0800

    606-设置主题颜色(21分钟) Done and 701 init 20190517

---
+++ 07/0701/News/News/Classes/Others/Constant.swift
@@ -0,0 +1,24 @@
+//
+//  Constant.swift
+//  News
+//
+//  Created by CoderDream on 2019/4/16.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+/// 屏幕的宽度
+let SCREEN_WIDTH = UIScreen.main.bounds.width
+/// 屏幕的宽度
+let SCREEN_HEIGHT = UIScreen.main.bounds.height
+
+let BASE_URL = "https://is.snssdk.com"
+let device_id: Int = 6096495334
+let iid: Int = 5034850950
+
+let kMyHeaderViewHeight: CGFloat = 280
+
+let isNight = "isNight"
+
+let isIPhoneX: Bool = SCREEN_HEIGHT == 812 ? true : false
---
+++ 07/0701/README.md
@@ -0,0 +1,12 @@
+
+Podfile
+```
+
+```
+
+
+添加 Build Phases -> Lind Binary With Libraries -> libsqlite3.0.tbd
+
+
+新建 Swift 文件：
+在 Main -> Tools 下新建 SQLiteManager.swift 文件

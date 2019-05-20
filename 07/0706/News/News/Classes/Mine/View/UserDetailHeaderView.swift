58483d3528185f3959e418b5832cf9783ab00f6c
CoderDream <coderdream@gmail.com>
2019-05-20 15:06:32 +0800

    705-自定义导航栏(24分钟) Done 706-添加相关推荐view(34分钟) init (课程针对iPhoneX，在6P上有问题) 20190520

---
+++ 07/0706/News/News/Classes/Mine/View/UserDetailHeaderView.swift
@@ -0,0 +1,288 @@
+//
+//  UserDetailHeaderView.swift
+//  News
+//
+//  Created by CoderDream on 2019/5/10.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+import IBAnimatable
+import Kingfisher
+
+class UserDetailHeaderView: UIView, NibLoadable {
+    /// 背景图片
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    /// 背景图片顶部约束
+    @IBOutlet weak var bgImageViewTop: NSLayoutConstraint!
+    /// 用户头像
+    @IBOutlet weak var avatarImageView: UIImageView!
+    /// V 图标
+    @IBOutlet weak var vImageView: UIImageView!
+    /// 用户名
+    @IBOutlet weak var nameLabel: UILabel!
+    /// 头条号图标
+    @IBOutlet weak var toutiaohaoImageView: UIImageView!
+    /// 发私信按钮
+    @IBOutlet weak var sendMailButton: UIButton!
+    /// 关注按钮
+    @IBOutlet weak var concernButton: AnimatableButton!
+    /// 推荐按钮
+    @IBOutlet weak var recommendButton: AnimatableButton!
+    @IBOutlet weak var recommendButtonWidth: NSLayoutConstraint!
+    @IBOutlet weak var recommendButtonTrailing: NSLayoutConstraint!
+    /// 推荐 view
+    @IBOutlet weak var recommendView: UIView!
+    @IBOutlet weak var recommendViewHeight: NSLayoutConstraint!
+    /// 头条认证
+    @IBOutlet weak var verifiedAgencyLabel: UILabel!
+    @IBOutlet weak var verifiedAgencyLabelHeight: NSLayoutConstraint!
+    @IBOutlet weak var verifiedAgencyLabelTop: NSLayoutConstraint!
+    /// 认证内容
+    @IBOutlet weak var verifiedContentLabel: UILabel!
+    /// 地区
+    @IBOutlet weak var areaButton: UIButton!
+    @IBOutlet weak var areaButtonHeight: NSLayoutConstraint!
+    @IBOutlet weak var areaButtonTop: NSLayoutConstraint!
+    /// 描述内容
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionLabelHeight: NSLayoutConstraint!
+    /// 展开按钮
+    @IBOutlet weak var unfoldButton: UIButton!
+    @IBOutlet weak var unfoldButtonWidth: NSLayoutConstraint!
+    @IBOutlet weak var unfoldButtonTrailing: NSLayoutConstraint!
+    /// 关注数量
+    @IBOutlet weak var followingsCountLabel: UILabel!
+    /// 粉丝数量
+    @IBOutlet weak var followersCountLabel: UILabel!
+    
+    // 文章 视频 问答
+    @IBOutlet weak var topTabView: UIView!
+    @IBOutlet weak var topTabHeight: NSLayoutConstraint!
+    @IBOutlet weak var separatorView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    /// 底层 view
+    @IBOutlet weak var baseView: UIView!
+    /// 底部的 ScrollView
+    //@IBOutlet weak var bottomScrollView: UIScrollView!
+    
+    /// 发私信按钮点击
+    @IBAction func sendMailButtonClicked() {
+        
+    }
+    
+    /// 关注按钮点击
+    @IBAction func concernButtonClicked(_ sender: AnimatableButton) {
+        print("concernButtonClicked")
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected { // 已经关注，点击则取消关注
+            // 取消关注
+            NetworkTool.loadRelationUnfollow(user_id: userDetail!.user_id, completionHandler: { (_) in
+                
+                self.concernButton.theme_backgroundColor = "colors.globalRedColor"
+                //sender.isSelected = !sender.isSelected
+                self.recommendButton.isHidden = true
+                self.recommendButton.isSelected = false
+                self.recommendButtonWidth.constant = 0
+                self.recommendButtonTrailing.constant = 0
+                self.recommendViewHeight.constant = 0
+                UIView.animate(withDuration: 0.25, animations: {
+                    self.recommendButton.imageView?.transform = .identity
+                    self.layoutIfNeeded()
+                })
+            })
+        } else { // 未关注，点击则关注
+            // 关注用户
+            NetworkTool.loadRelationFollow(user_id: userDetail!.user_id, completionHandler: { (_) in
+                self.concernButton.theme_backgroundColor = "colors.userDetailFollowingConcernBtnBgColor"
+                //sender.isSelected = !sender.isSelected
+                self.recommendButton.isHidden = false
+                self.recommendButton.isSelected = false
+                self.recommendButtonWidth.constant = 28.0
+                self.recommendButtonTrailing.constant = 15.0
+                self.recommendViewHeight.constant = 223
+                UIView.animate(withDuration: 0.25, animations: {
+                    self.layoutIfNeeded()
+                }, completion: { (_) in
+                    self.resetLayout()
+                    // 点击了关注按钮，就会出现相关推荐数据
+                    NetworkTool.loadRelationUserRecommend(user_id: self.userDetail!.user_id, completionHandler: { (userCard) in
+                        
+                    })
+                })
+            })
+        }
+        
+    }
+    
+    /// 推荐关注按钮点击
+    @IBAction func recommendButtonClicked(_ sender: AnimatableButton) {        
+        print("recommendButtonClicked")
+        sender.isSelected = !sender.isSelected
+        recommendViewHeight.constant = sender.isSelected ? 0 : 223.0
+        UIView.animate(withDuration: 0.25, animations: {
+            sender.imageView?.transform = CGAffineTransform(rotationAngle: CGFloat(sender.isSelected ? Double.pi : 0))
+            self.layoutIfNeeded()
+        }) { (_) in
+            self.resetLayout()
+        }
+    }
+    
+    /// 展开按钮点击
+    @IBAction func unfoldButtonClicked() {
+        unfoldButton.isHidden = true
+        unfoldButtonWidth.constant = 0
+        descriptionLabelHeight.constant = userDetail!.descriptionHeight!
+        
+        // UILabel填满一行后自动换行，三句代码搞定
+        // https://blog.csdn.net/haha223545/article/details/80111109
+        descriptionLabel.lineBreakMode = NSLineBreakMode.byWordWrapping;
+        descriptionLabel.numberOfLines = 0;
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.layoutIfNeeded()
+        }) { (_) in
+            self.resetLayout()
+        }
+    }
+    
+    /// 点击展开按钮后，刷新视图的高度
+    private func resetLayout()  {
+        baseView.height = topTabView.frame.maxY
+        height = baseView.frame.maxY
+    }
+    
+    var userDetail: UserDetail? {
+        didSet {
+            // 背景图片
+            backgroundImageView.kf.setImage(with: URL(string: userDetail!.bg_img_url)!)
+            // 头像
+            avatarImageView.kf.setImage(with: URL(string: userDetail!.avatar_url)!)
+            // VIP 图片
+            vImageView.isHidden = userDetail!.user_verified
+            nameLabel.text = userDetail!.screen_name
+            // 头条认证（如果为空，这约束（高度和top）设置为0，相当于隐藏）
+            if userDetail!.verified_agency == "" {
+                verifiedAgencyLabelHeight.constant = 0
+                verifiedAgencyLabelTop.constant = 0
+            } else {
+                verifiedAgencyLabel.text = userDetail!.verified_agency + "："
+                verifiedContentLabel.text = userDetail!.verified_content
+            } 
+            // 关注
+            concernButton.isSelected = userDetail!.is_following
+            // 设置颜色
+            concernButton.theme_backgroundColor = userDetail!.is_following ? "colors.userDetailFollowingConcernBtnBgColor" : "colors.globalRedColor"
+            concernButton.borderColor = userDetail!.is_following ? UIColor.grayColor232() : UIColor.globalRedColor()
+            concernButton.borderWidth = userDetail!.is_following ? 1 : 0
+            //
+            if userDetail!.area == "" {
+                areaButton.isHidden = true
+                areaButtonHeight.constant = 0
+                areaButtonTop.constant = 0
+            } else {
+                areaButton.setTitle(userDetail!.area, for: .normal)
+            }
+            // 描述
+            descriptionLabel.text = userDetail!.description
+            if userDetail!.descriptionHeight! > 21 {
+                unfoldButton.isHidden = false
+                unfoldButtonWidth.constant = 40
+            }
+            // 推荐按钮的约束 (如果已经关注，则不显示推荐按钮）
+            recommendButtonWidth.constant = 0
+            recommendButtonTrailing.constant = 10.0
+            
+            
+            followersCountLabel.text = userDetail!.followersCount
+            followingsCountLabel.text = userDetail!.followingsCount
+            //
+            if userDetail!.top_tab.count > 0 {
+                // 添加按钮
+                for (index, topTab) in userDetail!.top_tab.enumerated() {
+                    // 按钮
+                    let button = UIButton(frame: CGRect(x: CGFloat(index) * topTabButtonWidth, y: 0, width: topTabButtonWidth, height: scrollView.height))
+                    button.setTitle(topTab.show_name, for: .normal)
+                    button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+                    button.theme_setTitleColor("colors.black", forState: UIControl.State.normal)
+                    button.theme_setTitleColor("colors.globalRedColor", forState: UIControl.State.selected)
+                    button.addTarget(self, action: #selector(topTabButtonClicked), for: .touchUpInside)
+                    scrollView.addSubview(button)
+                    // 默认选中第一个
+                    if index == 0 {
+                        button.isSelected = true
+                        previewButton = button
+                    }
+                    if index == userDetail!.top_tab.count - 1 {
+                        scrollView.contentSize = CGSize(width: button.frame.maxX, height: scrollView.height)
+                    }
+                }
+                scrollView.addSubview(indicatorView)
+            } else {
+                topTabHeight.constant = 0
+                topTabView.isHidden = true
+            }
+            layoutIfNeeded()
+        }
+    }
+    
+    @objc func topTabButtonClicked(button: UIButton) {
+        previewButton?.isSelected = false
+        button.isSelected = !button.isSelected
+        UIView.animate(withDuration: 0.25, animations: {
+            self.indicatorView.centerX = button.centerX
+        }) { (_) in
+            self.previewButton = button
+        }
+    }
+    
+    // 指示条
+    private lazy var indicatorView: UIView = {
+        let indicatorView = UIView(frame: CGRect(x: (topTabButtonWidth - topTabIndicatorWidth) * 0.5, y: topTabView.height - 3, width: topTabIndicatorWidth, height: topTabIndicatorHeight))
+        indicatorView.theme_backgroundColor = "colors.globalRedColor"
+        return indicatorView
+    }()
+    
+    weak var previewButton = UIButton()
+    
+    @IBAction func sendMailButtonClick() {
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        concernButton.setTitle("关注", for: .normal)
+        concernButton.setTitle("已关注", for: .selected)
+        // 设置主题颜色
+        theme_backgroundColor = "colors.cellBackgroundColor"
+        baseView.theme_backgroundColor = "colors.cellBackgroundColor"
+        separatorView.theme_backgroundColor = "colors.separatorViewColor"
+        nameLabel.theme_textColor = "colors.black"
+        sendMailButton.theme_setTitleColor("colors.userDetailSendMailTextColor", forState: .normal)
+        unfoldButton.theme_setTitleColor("colors.userDetailSendMailTextColor", forState: .normal)
+        followersCountLabel.theme_textColor = "colors.userDetailSendMailTextColor"
+        followingsCountLabel.theme_textColor = "colors.userDetailSendMailTextColor"
+        concernButton.theme_setTitleColor("colors.userDetailConcernButtonTextColor", forState: .normal)
+        concernButton.theme_setTitleColor("colors.userDetailConcernButtonSelectedTextColor", forState: .selected)
+        verifiedAgencyLabel.theme_textColor = "colors.verifiedAgencyTextColor"
+        verifiedContentLabel.theme_textColor = "colors.black"
+        descriptionLabel.theme_textColor = "colors.black"
+        descriptionLabel.theme_textColor = "colors.black"
+        toutiaohaoImageView.theme_image = "images.toutiaohao"
+    }
+    
+//    class func headerView() -> UserDetailHeaderView {
+//        return Bundle.main.loadNibNamed("\(self)", owner: nil, options: nil)?.last as! UserDetailHeaderView
+//    }
+//
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
